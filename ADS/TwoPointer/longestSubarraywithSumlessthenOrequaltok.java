598b8d0a99fc0905dd036d211f73aaff94e96a08
Subhamstar <mr.subhamdas999@gmail.com>
2025-05-06 17:59:40 +0530

    maxlen updated

--- ADS/TwoPointer/longestSubarraywithSumlessthenOrequaltok.java
+++ ADS/TwoPointer/longestSubarraywithSumlessthenOrequaltok.java
@@ -5,22 +5,22 @@ public class longestSubarraywithSumlessthenOrequaltok {
         int sum=0;
         int l=0,r=0;
         int n=arr.length;
-        int maxsum=0;
+        int maxlen=0;
         while(r<n){
             sum+=arr[r];
-            r++;
             if(sum<=k)
-            maxsum=Math.max(r-l,maxsum);
-            else{
+            maxlen=Math.max(r-l+1,maxlen);
+            while(sum>k){
+                sum-=arr[l];
                 l++;
             }
-
+            r++;
         }
-        return maxsum;
+        return maxlen;
     }
     public static void main(String[] args) {
-        int arr[]={2,5,1,7,10};
-        int k=7;
+        int arr[]={2,5,1,7,10}; 
+        int k=14;
         System.out.println(maximum(arr,k));
     }
 }
